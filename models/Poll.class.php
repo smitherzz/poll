77b111e40b09c467ebfc0be7f43b2601feec8901
smitherzz <thimbletemail@gmail.com>
2017-07-09 16:56:52 -0400

    Create poll model class

---
+++ models/Poll.class.php
@@ -0,0 +1,28 @@
+<?php
+class Poll {
+  private $db;
+  public function __construct( $dbConnection ){
+   $this->db = $dbConnection;
+  }
+  public function updatePoll ($input) {
+    if ($input === "yes"){
+      $updateSQL = "UPDATE poll SET yes = yes+1 WHERE poll_id = 1";
+    } else if ($input === "no"){
+      $updateSQL = "UPDATE poll SET no = no+1 WHERE poll_id = 1";
+    }
+    $updateStatement = $this->db->prepare($updateSQL);
+    $updateStatement->execute();
+  }
+  public function getPollData() {
+    //the actual SQL statement
+    $sql = "SELECT poll_question, yes, no FROM poll WHERE poll_id = 1";
+    //Use PDO connection to create a PDOStatement object
+    $statement = $this->db->prepare($sql);
+    //tell MySQL to execute the statement
+    $statement->execute();
+    //retrieve the first row of data from the table
+    $pollData = $statement->fetchObject();
+    //make poll data available to the caller
+    return $pollData;
+  }
+}
